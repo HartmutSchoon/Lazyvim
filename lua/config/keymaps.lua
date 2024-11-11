2b9ae831f009950b576363702f5f04217f3ad073
Hartmut Schoon <hartmut.schoon@outlook.de>
2024-11-11 15:21:20 +0100

    fixed DAP inlay hints

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -41,3 +41,180 @@ map("n", "<leader>Tm", "<cmd>MarkdownPreview<cr>", { desc = "Markdown preview" }
 
 -- Telescope undo
 map("n", "<leader>U", "<cmd>Telescope undo<cr>", { desc = "Undo history" })
+
+--  Register keymap with which-key
+local wk = require("which-key")
+wk.add({
+  --DAP
+  {
+    "<F5>",
+    function()
+      require("dap").continue()
+    end,
+    desc = "Debugger: Run",
+  },
+  {
+    "<F17>",
+    function()
+      require("dap").terminate()
+    end,
+    desc = "Debugger: Stop",
+  },
+
+  {
+    "<F21>", -- Shift+F9
+    function()
+      require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+    end,
+    desc = "Debugger: Conditional Breakpoint",
+  },
+  {
+    "<F29>",
+    function()
+      require("dap").restart_frame()
+    end,
+    desc = "Debugger: Restart",
+  }, -- Control+F5
+  {
+    "<F6>",
+    function()
+      require("dap").pause()
+    end,
+    desc = "Debugger: Pause",
+  },
+  {
+    "<F9>",
+    function()
+      require("dap").toggle_breakpoint()
+    end,
+    desc = "Debugger: Toggle Breakpoint",
+  },
+  {
+    "<F10>",
+    function()
+      require("dap").step_over()
+    end,
+    desc = "Debugger: Step Over",
+  },
+  {
+    "<F11>",
+    function()
+      require("dap").step_into()
+    end,
+    desc = "Debugger: Step Into",
+  },
+  {
+    "<F23>",
+    function()
+      require("dap").step_out()
+    end,
+    desc = "Debugger: Step Out",
+  }, -- Shift+F11
+
+  { "<leader>du", '<cmd>lua require("dapui").toggle() <cr>', desc = "Toggle UI" },
+  {
+    "<leader>dh",
+    function()
+      require("dap.ui.widgets").hover()
+    end,
+    desc = "Debugger Hover",
+  },
+  {
+    "<leader>dE",
+    function()
+      require("dapui").eval()
+    end,
+    desc = "Evaluate Input",
+  },
+  {
+    "<leader>db",
+    function()
+      require("dap").toggle_breakpoint()
+    end,
+    desc = "Toggle Breakpoint (F9)",
+  },
+  {
+    "<leader>dB",
+    function()
+      require("dap").clear_breakpoints()
+    end,
+    desc = "Clear Breakpoints",
+  },
+  {
+    "<leader>dc",
+    function()
+      require("dap").continue()
+    end,
+    desc = "Start/Continue (F5)",
+  },
+  {
+    "<leader>dC",
+    function()
+      require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+    end,
+    desc = "Conditional Breakpoint (S-F9)",
+  },
+  {
+    "<leader>di",
+    function()
+      require("dap").step_into()
+    end,
+    desc = "Step Into (F11)",
+  },
+  {
+    "<leader>do",
+    function()
+      require("dap").step_over()
+    end,
+    desc = "Step Over (F10)",
+  },
+  {
+    "<leader>dO",
+    function()
+      require("dap").step_out()
+    end,
+    desc = "Step Out (S-F11)",
+  },
+  {
+    "<leader>dq",
+    function()
+      require("dap").close()
+    end,
+    desc = "Close Session",
+  },
+  {
+    "<leader>dQ",
+    function()
+      require("dap").terminate()
+    end,
+    desc = "Terminate Session (S-F5)",
+  },
+  {
+    "<leader>dp",
+    function()
+      require("dap").pause()
+    end,
+    desc = "Pause (F6)",
+  },
+  {
+    "<leader>dr",
+    function()
+      require("dap").restart_frame()
+    end,
+    desc = "Restart (C-F5)",
+  },
+  {
+    "<leader>dR",
+    function()
+      require("dap").repl.toggle()
+    end,
+    desc = "Toggle REPL",
+  },
+  {
+    "<leader>ds",
+    function()
+      require("dap").run_to_cursor()
+    end,
+    desc = "Run To Cursor",
+  },
+})
